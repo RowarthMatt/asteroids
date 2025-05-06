08f3e32a466ecefda029e5870c144db8b352b3eb
RowarthMatt <rowarthmatt@gmail.com>
2025-05-06 08:32:14 +0100

    lesson 3.2

--- main.py
+++ main.py
@@ -2,22 +2,39 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import sys  
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import * 
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    af = AsteroidField()
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game Over!")
+                sys.exit()
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
